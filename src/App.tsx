3289d3ef49de87a4c3593283772202149e09c5b3
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-03-29 22:26:27 +0400

    sketch (11/100)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App: React.FC = () => {
+   return (
+      <div className="wrapper">
+         <header className="header">
+            <img src="https://www.reactacademy.live/logoReact300px.png" alt="Logo" />
+         </header>
+         <nav className="nav">
+            <div>
+               <a href="#profile">Profile</a>
+            </div>
+            <div>
+               <a href="#messages">Messages</a>
+            </div>
+            <div>
+               <a href="#news">News</a>
+            </div>
+            <div>
+               <a href="#music">Music</a>
+            </div>
+            <div>
+               <a href="#settings">Settings</a>
+            </div>
+         </nav>
+         <div className="content">
+            <div>
+               <img src="https://i.ibb.co/CQyysgB/pexels-photo-248797.jpg" alt="wallpaper" />
+            </div>
+            <div>
+               ava + description
+            </div>
+            <div>
+               My posts
+               <div>
+                  New post
+               </div>
+               <div>
+                  <div>
+                     post1
+                  </div>
+                  <div>
+                     post2
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
 }
 
-export default App;
+export default App
