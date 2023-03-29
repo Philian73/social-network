dc3ffb995e3fd41d0ce042c032e06b8eacc673d6
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-03-29 23:10:50 +0400

    division into component files (12/100)

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header: React.FC = () => {
+   return (
+      <header className="header">
+         <img src="https://www.reactacademy.live/logoReact300px.png" alt="Logo" />
+      </header>
+   )
+}
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const Profile: React.FC = () => {
+   return (
+      <div className="content">
+         <div>
+            <img src="https://i.ibb.co/CQyysgB/pexels-photo-248797.jpg" alt="wallpaper" />
+         </div>
+         <div>
+            ava + description
+         </div>
+         <div>
+            My posts
+            <div>
+               New post
+            </div>
+            <div>
+               <div>
+                  post1
+               </div>
+               <div>
+                  post2
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
