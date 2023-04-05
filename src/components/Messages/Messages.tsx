57872894fef102c0a20c04da5044c4adfb93b818
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-05 18:38:43 +0400

    Outline layout of 'Messages'. (21/100)

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
-//import s from './Messages.module.css'
+import s from './Messages.module.css'
 
 export const Messages: React.FC = () => {
    return (
-      <span>Messages</span>
+      <div className={s.messages}>
+         <div className={s.dialogsItems}>
+            <div className={`${s.dialog} ${s.active}`}>
+               Nikolay
+            </div>
+            <div className={s.dialog}>
+               Darya
+            </div>
+            <div className={s.dialog}>
+               Nikita
+            </div>
+            <div className={s.dialog}>
+               Maxim
+            </div>
+            <div className={s.dialog}>
+               Vasiliy
+            </div>
+            <div className={s.dialog}>
+               Andrei
+            </div>
+         </div>
+         <div className={s.chat}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are you doing?</div>
+            <div className={s.message}>See you?</div>
+         </div>
+      </div>
    )
 }
