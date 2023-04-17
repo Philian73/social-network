51c82081b501f30a5caf98823770ccb748ad7c0e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-17 23:49:14 +0400

    created state (not redux lib)

---
+++ src/redux/state.tsx
@@ -0,0 +1,85 @@
+export type PostType = {
+   id: number
+   message: string
+   likesCount: number
+}
+
+export type MessageType = {
+   message: string
+   id: number
+}
+
+export type DialogType = {
+   id: number
+   name: string
+}
+
+export type FriendsType = {
+   id: number
+   nameFriend: string
+   avatar: string
+}
+
+export type MessagesPageType = {
+   dialogs: DialogType[]
+   messages: MessageType[]
+}
+
+export type ProfilePageType = {
+   posts: PostType[]
+}
+
+export type SidebarType = {
+   friends: FriendsType[]
+}
+
+export type RootStateType = {
+   profilePage: ProfilePageType
+   messagesPage: MessagesPageType
+   sidebar: SidebarType
+}
+
+export const state: RootStateType = {
+   profilePage: {
+      posts: [
+         {id: 1, message: 'Hi, how are you?', likesCount: 15},
+         {id: 2, message: 'It\'s my first post', likesCount: 20},
+      ],
+   },
+   messagesPage: {
+      dialogs: [
+         {id: 1, name: 'Nikolay'},
+         {id: 2, name: 'Darya'},
+         {id: 3, name: 'Nikita'},
+         {id: 4, name: 'Maxim'},
+         {id: 5, name: 'Vasiliy'},
+         {id: 6, name: 'Andrei'},
+      ],
+      messages: [
+         {id: 1, message: 'Hi'},
+         {id: 2, message: 'How are you doing?'},
+         {id: 3, message: 'See you?'},
+         {id: 4, message: 'Yo'},
+         {id: 5, message: 'Yo'},
+      ],
+   },
+   sidebar: {
+      friends: [
+         {
+            id: 1,
+            nameFriend: 'Darya',
+            avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+         },
+         {
+            id: 2,
+            nameFriend: 'Maxim',
+            avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+         },
+         {
+            id: 3,
+            nameFriend: 'Nikita',
+            avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+         },
+      ],
+   },
+}
