59dda9f35868f053b156175e88f0d2f7ff81bd78
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-14 23:16:30 +0400

    Added a new test reducer (42/100)

---
+++ src/redux/sidebarReducer.ts
@@ -0,0 +1,33 @@
+import {ActionsType} from './store'
+
+export type FriendType = {
+   id: number
+   nameFriend: string
+   avatar: string
+}
+
+export type SidebarType = {
+   friends: FriendType[]
+}
+
+const initialState: SidebarType = {
+   friends: [
+      {
+         id: 1,
+         nameFriend: 'Darya',
+         avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+      },
+      {
+         id: 2,
+         nameFriend: 'Maxim',
+         avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+      },
+      {
+         id: 3,
+         nameFriend: 'Nikita',
+         avatar: 'https://avatars.mds.yandex.net/i?id=2bf8c047d61a77491675e765c2a8f65d538037fe-9182388-images-thumbs&n=13'
+      },
+   ],
+}
+
+export const sidebarReducer = (state = initialState, action: ActionsType) => state
