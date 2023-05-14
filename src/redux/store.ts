92675146f8c590bebe42d553754113489aa6e4f8
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-14 23:16:12 +0400

    Added a new Store (redux-store) (42/100)

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import {combineReducers, createStore} from 'redux'
+
+import {ProfileActionsType, profileReducer} from './profileReducer'
+import {MessagesActionsType, messagesReducer} from './messagesReducer'
+import {sidebarReducer} from './sidebarReducer'
+
+export type ActionsType = ProfileActionsType | MessagesActionsType
+
+export type AppStateType = ReturnType<typeof rootReducer>
+
+const rootReducer = combineReducers({
+   profilePage: profileReducer,
+   messagesPage: messagesReducer,
+   sidebar: sidebarReducer,
+})
+
+export const store = createStore(rootReducer)
