417e1364bb21b73bdf5169d0107bce3925afb7c3
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-15 16:42:36 +0400

    No longer passing "Store" through the tree (44/100)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, {FC} from 'react'
+import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 
 import './styles/App.css'
-
-import {StoreType} from './redux/store'
 
 import {Header} from './components/Header/Header'
 import {Navbar} from './components/Navbar/Navbar'
@@ -13,20 +11,17 @@ import {Music} from './components/Music/Music'
 import {Settings} from './components/Settings/Settings'
 import {MessagesContainer} from './components/Messages/MessagesContainer'
 
-type PropsType = {
-   store: StoreType
-}
-const App: FC<PropsType> = ({store}) => {
+const App = () => {
    return (
       <div className="wrapper">
          <Header />
-         <Navbar sidebar={store.getState().sidebar} />
+         <Navbar />
          <div className="app-wrapper-content">
             <Switch>
                <Route path="/profile"
-                      render={() => <Profile store={store} />} />
+                      render={() => <Profile />} />
                <Route path="/messages"
-                      render={() => <MessagesContainer store={store} />} />
+                      render={() => <MessagesContainer />} />
                <Route path="/news"
                       render={() => <News />} />
                <Route path="/music"
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,16 @@
-import React, {FC} from 'react'
+import React from 'react'
 
 // import s from './Profile.module.css'
-
-import {StoreType} from '../../redux/store'
 
 import {ProfileInfo} from './ProfileInfo/ProfileInfo'
 import {MyPostsContainer} from './MyPosts/MyPostsContainer'
 
-type PropsType = {
-   store: StoreType
-}
-export const Profile: FC<PropsType> = ({store}) => {
+
+export const Profile = () => {
    return (
       <div>
          <ProfileInfo />
-         <MyPostsContainer store={store} />
+         <MyPostsContainer />
       </div>
    )
 }
