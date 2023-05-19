fd58769a1022741207eff7bbba54d85ef134020e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-19 16:32:43 +0400

    removed StoreType and removed sidebarReducer (45/100)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,18 +2,14 @@ import { combineReducers, createStore } from 'redux'
 
 import { profileReducer } from './profileReducer'
 import { messagesReducer } from './messagesReducer'
-import { sidebarReducer } from './sidebarReducer'
 
 export type InferActionTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
-
-export type StoreType = typeof store
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
 const rootReducer = combineReducers({
    profilePage: profileReducer,
    messagesPage: messagesReducer,
-   sidebar: sidebarReducer,
 })
 
 export const store = createStore(rootReducer)
