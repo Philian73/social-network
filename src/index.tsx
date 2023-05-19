a08249023a91f5882f8a13ca82a66a307a6f6b64
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-19 19:09:11 +0400

    removed custom subscribe and rerenderEntireTree (47/100)

--- src/index.tsx
+++ src/index.tsx
@@ -9,17 +9,11 @@ import { store } from './redux/store'
 
 import App from './App'
 
-const rerenderEntireTree = () => {
-   ReactDOM.render(
-      <BrowserRouter>
-         <Provider store={store}>
-            <App />
-         </Provider>
-      </BrowserRouter>,
-      document.getElementById('root')
-   )
-}
-
-rerenderEntireTree()
-
-store.subscribe(() => rerenderEntireTree())
+ReactDOM.render(
+   <BrowserRouter>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </BrowserRouter>,
+   document.getElementById('root') as HTMLElement
+)
