407f334a7f5d04b79320399c26517d8074a9f981
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-25 23:26:39 +0400

    Added authReducer.ts (61/100)

---
+++ src/redux/authReducer.ts
@@ -0,0 +1,34 @@
+import { InferActionTypes } from './store'
+
+type ActionsType = InferActionTypes<typeof actions>
+
+type InitialStateType = typeof initialState
+
+const initialState = {
+   id: null as (number | null),
+   email: null as (string | null),
+   login: null as (string | null),
+   isAuth: false,
+}
+
+export const AuthReducer = (state = initialState, action: ActionsType): InitialStateType => {
+   switch (action.type) {
+      case 'SET-AUTH-USER-DATA':
+         return {
+            ...state,
+            ...action.payload.data,
+            isAuth: true
+         }
+      default:
+         return state
+   }
+}
+
+export const actions = {
+   setAuthUserData: (id: number, email: string, login: string) => ({
+      type: 'SET-AUTH-USER-DATA',
+      payload: {
+         data: { id, email, login }
+      }
+   } as const)
+}
