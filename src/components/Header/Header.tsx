9e1497a555ef42b876d1f0177dd9945da3348af1
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-25 23:28:00 +0400

    Link (react-router-dom) and props from ContainerComponent (61/100)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+
 import s from './Header.module.css'
 
-export const Header = () => {
+import { HeaderPropsType } from './HeaderContainer'
+
+export const Header: FC<HeaderPropsType> = ({ isAuth, login }) => {
    return (
       <header className={s.header}>
          <img src="https://www.reactacademy.live/logoReact300px.png" alt="Logo" />
+         <div className={s.loginBlock}>
+            {isAuth ? (
+               <span>{login}</span>
+            ) : (
+               <Link to="/login">Login</Link>
+            )}
+         </div>
       </header>
    )
 }
