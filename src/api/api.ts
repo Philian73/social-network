1fc1a1c7b62d4b2c0507dadb8bff4683d3bb39d5
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-26 23:57:16 +0300

    refactor (+then data) (63/100)

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,5 +5,6 @@ export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
       .get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
          withCredentials: true,
       })
+      .then(response => response.data)
 }
 
