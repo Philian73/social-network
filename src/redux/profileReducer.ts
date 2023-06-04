48f7a96ea84152720e00b14f003fe97cf592c1ef
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-04 23:58:30 +0300

    created thunk 'getUserProfile' (67/100)

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,5 +1,7 @@
 import { InferActionTypes } from './store'
 import { PostType, ProfileType } from './types'
+import { Dispatch } from 'redux'
+import { usersAPI } from '../api/usersAPI'
 
 type ActionsType = InferActionTypes<typeof actions>
 
@@ -39,4 +41,13 @@ export const actions = {
    updateNewPostText: (text: string) => ({ type: 'UPDATE-NEW-POST-TEXT', payload: { newText: text } } as const),
    addPost: () => ({ type: 'ADD-POST' } as const),
    setUserProfile: (profile: ProfileType) => ({ type: 'SET-USER-PROFILE', payload: { profile } } as const),
+}
+
+export const getUserProfile = (userID: number) => {
+   return (dispatch: Dispatch) => {
+      usersAPI.getProfile(userID)
+         .then(response => {
+            dispatch(actions.setUserProfile(response.data))
+         })
+   }
 }
