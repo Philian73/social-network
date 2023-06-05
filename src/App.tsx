cb3901658e4b73a335f84e097e65820a0053a534
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-05 23:33:55 +0400

    Added router '/login' and import Login.tsx (68/100)

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { MessagesContainer } from './components/Messages/MessagesContainer'
 import { UsersContainer } from './components/Users/UsersContainer'
 import { ProfileContainer } from './components/Profile/ProfileContainer'
 import { HeaderContainer } from './components/Header/HeaderContainer'
+import { Login } from './components/Login/Login'
 
 const App = () => {
    return (
@@ -31,6 +32,8 @@ const App = () => {
                       render={() => <Music />} />
                <Route path="/settings"
                       render={() => <Settings />} />
+               <Route path="/login"
+                      render={() => <Login />} />
             </Switch>
          </div>
       </div>
