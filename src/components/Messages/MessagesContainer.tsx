abdf5f348fe7fc0631c1c154e4bb43d23ce18016
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-22 23:26:07 +0300

    refactor: compose (70/100)

--- src/components/Messages/MessagesContainer.tsx
+++ src/components/Messages/MessagesContainer.tsx
@@ -1,20 +1,25 @@
+import { ComponentType } from 'react'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 
-import { AppStateType } from '../../redux/store'
-import { actions } from '../../redux/messagesReducer'
+import { AppStateType } from 'redux/store'
+import { actions } from 'redux/messagesReducer'
 
 import { Messages } from './Messages'
-import { withAuthRedirect } from '../../hoc/withAuthRedirect'
-
-type MapStatePropsType = ReturnType<typeof mapStateToProps>
-type MapDispatchPropsType = typeof actions
-
-export type MessagesPropsType = MapDispatchPropsType & MapStatePropsType
+import { withAuthRedirect } from 'hoc/withAuthRedirect'
 
 const mapStateToProps = (state: AppStateType) => ({
    messagesPage: state.messagesPage,
 })
 
-const AuthRedirectComponent = withAuthRedirect(Messages)
+export const MessagesContainer = compose<ComponentType>(
+   connect(mapStateToProps, { ...actions }),
+   withAuthRedirect,
+)(Messages)
 
-export const MessagesContainer = connect(mapStateToProps, { ...actions })(AuthRedirectComponent)
+
+// TYPES
+type MapStatePropsType = ReturnType<typeof mapStateToProps>
+type MapDispatchPropsType = typeof actions
+
+export type MessagesPropsType = MapDispatchPropsType & MapStatePropsType
