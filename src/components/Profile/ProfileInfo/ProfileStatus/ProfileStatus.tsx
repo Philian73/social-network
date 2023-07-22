bd5048645a8aacca1efeef71a6cb3631677b91da
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-23 00:57:06 +0300

    activateEditMode, deactivateEditMode, onFocusHandler. PropsType, StateType (71/100)

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,18 +1,48 @@
-import { FC } from 'react'
+import { Component, FocusEvent } from 'react'
+
+export class ProfileStatus extends Component<PropsType, StateType> {
+   state: StateType = {
+      editMode: false
+   }
+
+   activateEditMode = () => {
+      !this.state.editMode && this.setState({ editMode: true })
+   }
+
+   deactivateEditMode = () => {
+      this.state.editMode && this.setState({ editMode: false })
+   }
+
+   onFocusHandler = (e: FocusEvent<HTMLInputElement>) => {
+      e.target.select()
+   }
+
+   render() {
+      const { status } = this.props
+      const { editMode } = this.state
+      const { activateEditMode, deactivateEditMode, onFocusHandler } = this
+
+      return !editMode ? (
+         <div>
+            <span onDoubleClick={activateEditMode}>{status}</span>
+         </div>
+      ) : (
+         <div>
+            <input value={status}
+                   autoFocus
+                   onFocus={onFocusHandler}
+                   onBlur={deactivateEditMode}
+            />
+         </div>
+      )
+   }
+}
 
 
+// TYPES
 type PropsType = {
    status: string
 }
-export const ProfileStatus: FC<PropsType> = ({ status }) => {
-   return (
-      <div>
-         <div>
-            <span>{status}</span>
-         </div>
-         <div>
-            <input value={status}></input>
-         </div>
-      </div>
-   )
+type StateType = {
+   editMode: boolean
 }
