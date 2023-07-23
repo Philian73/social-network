25445ad274cd567a2efbd9cacf3ff5b3b4b6ed58
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-23 23:47:19 +0300

    layout (75/100)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,22 @@
+import cls from './Login.module.css'
+
 export const Login = () => {
-   return <h1>LOGIN</h1>
+   return (
+      <div>
+         <h1>Login</h1>
+         <form className={cls.form}>
+            <label htmlFor="login">
+               <input placeholder="Login" type="text" id="login" />
+            </label>
+            <label htmlFor="password">
+               <input placeholder="Password" type="password" id="password" />
+            </label>
+            <label htmlFor="rememberMe">
+               <input type="checkbox" id="rememberMe" name="rememberMe" />
+               <span>Remember Me</span>
+            </label>
+            <button>Log In</button>
+         </form>
+      </div>
+   )
 }
