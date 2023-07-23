23521dde36922f146d24582edcbdccd5bda8edd8
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-23 23:59:34 +0300

    import LoginReduxForm.tsx component (75/100)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,22 +1,10 @@
-import cls from './Login.module.css'
+import { LoginReduxForm } from 'components/Login/LoginForm/LoginForm'
 
 export const Login = () => {
    return (
       <div>
          <h1>Login</h1>
-         <form className={cls.form}>
-            <label htmlFor="login">
-               <input placeholder="Login" type="text" id="login" />
-            </label>
-            <label htmlFor="password">
-               <input placeholder="Password" type="password" id="password" />
-            </label>
-            <label htmlFor="rememberMe">
-               <input type="checkbox" id="rememberMe" name="rememberMe" />
-               <span>Remember Me</span>
-            </label>
-            <button>Log In</button>
-         </form>
+         <LoginReduxForm />
       </div>
    )
 }
