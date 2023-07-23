cf36b6298eacd9b5ddbc30bd67d4523298cf17fe
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-23 19:56:53 +0300

    refactor + setStatus case + status to state added methods: getStatus & updateStatus (73/100)

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -2,6 +2,7 @@ import { InferActionTypes } from './store'
 import { PostType, ProfileType } from './types'
 import { Dispatch } from 'redux'
 import { profileAPI } from 'api/profileAPI'
+import { APIResultCodes } from 'api/api'
 
 type ActionsType = InferActionTypes<typeof actions>
 
@@ -14,6 +15,7 @@ const initialState = {
    ] as PostType[],
    newPostText: '',
    profile: null as ProfileType | null,
+   status: ''
 }
 
 export const profileReducer = (state = initialState, action: ActionsType): InitialStateType => {
@@ -32,6 +34,8 @@ export const profileReducer = (state = initialState, action: ActionsType): Initi
          }
       case 'SET-USER-PROFILE':
          return { ...state, profile: action.payload.profile }
+      case 'SET-STATUS':
+         return { ...state, status: action.payload.status }
       default:
          return state
    }
@@ -41,13 +45,35 @@ export const actions = {
    updateNewPostText: (text: string) => ({ type: 'UPDATE-NEW-POST-TEXT', payload: { newText: text } } as const),
    addPost: () => ({ type: 'ADD-POST' } as const),
    setUserProfile: (profile: ProfileType) => ({ type: 'SET-USER-PROFILE', payload: { profile } } as const),
+   setStatus: (status: string) => ({ type: 'SET-STATUS', payload: { status } } as const),
 }
 
-export const getUserProfile = (userID: number) => {
-   return (dispatch: Dispatch) => {
-      profileAPI.getProfile(userID)
-         .then(response => {
-            dispatch(actions.setUserProfile(response.data))
-         })
-   }
+export const thunks = {
+   getUserProfile(userID: number) {
+      return (dispatch: Dispatch) => {
+         profileAPI.getProfile(userID)
+            .then(response => {
+               dispatch(actions.setUserProfile(response.data))
+            })
+      }
+   },
+   getStatus(userID: number) {
+      return (dispatch: Dispatch) => {
+         profileAPI.getStatus(userID)
+            .then(response => {
+               dispatch(actions.setStatus(response.data))
+            })
+      }
+   },
+   updateStatus(status: string) {
+      return (dispatch: Dispatch) => {
+         profileAPI.updateStatus(status)
+            .then(response => {
+               if (response.data.resultCode === APIResultCodes.SUCCESS) {
+                  dispatch(actions.setStatus(status))
+               }
+            })
+      }
+   },
 }
+
