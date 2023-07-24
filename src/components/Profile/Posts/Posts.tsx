56414c64d8783364814bd34020a9934c3caf19e1
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-24 20:55:42 +0300

    refactor (76/100)

--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -1,14 +1,16 @@
-import { ChangeEvent, FC } from 'react'
+import { FC } from 'react'
 
 import { PostsPropsType } from './PostsContainer'
-
 import { Post } from './Post/Post'
+import { AddPostFormData, AddPostReduxForm } from 'components/Profile/Posts/AddPostForm/AddPostForm'
 
 import s from './Posts.module.css'
 
-export const Posts: FC<PostsPropsType> = ({ posts, newPostText, updateNewPostText, addPost }) => {
-   const onNewPostTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => updateNewPostText(e.currentTarget.value)
-   const onAddPost = () => addPost()
+
+export const Posts: FC<PostsPropsType> = ({ posts, addPost }) => {
+   const addNewPost = (formData: AddPostFormData) => {
+      addPost(formData.newPostText)
+   }
 
    const postsMap = posts
       .map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount} />)
@@ -16,14 +18,7 @@ export const Posts: FC<PostsPropsType> = ({ posts, newPostText, updateNewPostTex
    return (
       <div className={s.postsBlock}>
          <h3>My posts</h3>
-         <div>
-            <div>
-               <textarea onChange={onNewPostTextChange} value={newPostText} />
-            </div>
-            <div>
-               <button onClick={onAddPost}>Add post</button>
-            </div>
-         </div>
+         <AddPostReduxForm onSubmit={addNewPost} />
          <div className={s.posts}>
             {postsMap}
          </div>
