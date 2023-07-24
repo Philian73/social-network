09e342f79e698234ced54d1b028162acae9c2c91
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-24 23:40:57 +0300

    Created ui TextArea component (77/100)

---
+++ src/components/common/Textarea/Textarea.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+import { WrappedFieldProps } from 'redux-form/lib/Field'
+
+import cls from './Textarea.module.css'
+
+export const Textarea: FC<WrappedFieldProps> = ({ input, meta, ...rest }) => {
+   const hasError = !meta.pristine && meta.error
+   const hasWarn = !meta.pristine && meta.warning
+
+   const textareaStyles = cls.formControl + (hasError ? ' ' + cls.error : '')
+
+   return (
+      <div className={textareaStyles}>
+         <textarea {...input} {...rest} />
+         {hasError || hasWarn && <span>{meta.warning}</span>}
+      </div>
+   )
+}
