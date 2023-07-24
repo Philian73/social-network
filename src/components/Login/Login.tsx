80ab4e155db77f654d6ce1e3a57b96c03b3b0405
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-24 19:27:18 +0300

    temp 'onSubmit' and import LoginParamsType (75/100)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,14 @@
 import { LoginReduxForm } from 'components/Login/LoginForm/LoginForm'
+import { LoginParamsType } from 'api/authAPI'
 
 export const Login = () => {
+   const onSubmit = (formData: LoginParamsType) => {
+      console.log(formData)
+   }
    return (
       <div>
          <h1>Login</h1>
-         <LoginReduxForm />
+         <LoginReduxForm onSubmit={onSubmit} />
       </div>
    )
 }
