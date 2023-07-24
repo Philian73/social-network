d64a7f995f721da3dc112681e74071fd79454134
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-24 23:40:25 +0300

    added AddMessageForm component (77/100)

--- src/components/Messages/AddMessageForm/AddMessageForm.tsx
+++ src/components/Messages/AddMessageForm/AddMessageForm.tsx
@@ -1,11 +1,12 @@
 import { Field, reduxForm } from 'redux-form'
+import { Textarea } from 'components/common/Textarea/Textarea'
 
 export const AddMessageReduxForm = reduxForm<AddMessageFormData>({
    form: 'dialogAddMessageForm',
 })(({ handleSubmit }) => {
    return (
       <form onSubmit={handleSubmit}>
-         <Field component="textarea"
+         <Field component={Textarea}
                 name="newMessageBody"
                 placeholder="Enter your message"
          />
