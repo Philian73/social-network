47a0b161d465eca195b2f68d56f61e03f3ce950e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-25 23:58:47 +0300

    realized (77/100)

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react'
+import { WrappedFieldProps } from 'redux-form/lib/Field'
 
-type PropsType = {}
-export const Input: FC<PropsType> = ({}) => {
+import cls from './Input.module.css'
+
+export const Input: FC<WrappedFieldProps> = ({ input, meta, ...rest }) => {
+   const hasError = !meta.touched && meta.error
+
+   const inputStyles = cls.formControl + (hasError ? ' ' + cls.error : '')
 
    return (
-      <div>
-
+      <div className={inputStyles}>
+         <input {...input} {...rest} />
+         {hasError && <span>{meta.error}</span>}
       </div>
    )
 }
