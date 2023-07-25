86b9b5e6f294bb3f807efb65d4b5671027d3da67
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-25 23:41:24 +0200

    added isValidEmail (77/100)

--- src/utils/validators/index.ts
+++ src/utils/validators/index.ts
@@ -7,3 +7,7 @@ export const maxLength = (num: number) => {
 export const minLength = (num: number) => {
    return (value: string) => value && value.length < num ? `Must be at least ${num} characters long` : undefined
 }
+
+export const isValidEmail = (email: string) => {
+   return !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email) ? 'Invalid email address' : undefined
+}
