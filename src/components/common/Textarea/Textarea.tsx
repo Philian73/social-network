f9d72bc061fafa898a64642c13a0cf32c166fab9
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-25 23:44:22 +0300

    refactor (77/100)

--- src/components/common/Textarea/Textarea.tsx
+++ src/components/common/Textarea/Textarea.tsx
@@ -5,14 +5,13 @@ import cls from './Textarea.module.css'
 
 export const Textarea: FC<WrappedFieldProps> = ({ input, meta, ...rest }) => {
    const hasError = !meta.pristine && meta.error
-   const hasWarn = !meta.pristine && meta.warning
 
    const textareaStyles = cls.formControl + (hasError ? ' ' + cls.error : '')
 
    return (
       <div className={textareaStyles}>
          <textarea {...input} {...rest} />
-         {(hasError || hasWarn) && <span>{meta.warning}</span>}
+         {hasError && <span>{meta.error}</span>}
       </div>
    )
 }
