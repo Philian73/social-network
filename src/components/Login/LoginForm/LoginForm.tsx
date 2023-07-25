4dfa326ed5f73cd7b22b6459d8c239d7c563664a
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-25 23:41:47 +0200

    import and used isValidEmail (77/100)

--- src/components/Login/LoginForm/LoginForm.tsx
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -1,8 +1,13 @@
 import { Field, reduxForm } from 'redux-form'
 
-import cls from 'components/Login/LoginForm/LoginForm.module.css'
 import { LoginParamsType } from 'api/authAPI'
 import { Input } from 'components/common/Input/Input'
+
+import { isValidEmail, minLength, requiredField } from 'utils/validators'
+
+import cls from 'components/Login/LoginForm/LoginForm.module.css'
+
+const minPasswordLength = minLength(4)
 
 export const LoginReduxForm = reduxForm<LoginParamsType>({
    form: 'login',
@@ -11,6 +16,7 @@ export const LoginReduxForm = reduxForm<LoginParamsType>({
       <form onSubmit={handleSubmit} className={cls.form}>
          <label htmlFor="email">
             <Field component={Input}
+                   validate={[requiredField, isValidEmail]}
                    type="text"
                    id="email"
                    name="email"
@@ -19,13 +25,14 @@ export const LoginReduxForm = reduxForm<LoginParamsType>({
          </label>
          <label htmlFor="password">
             <Field component={Input}
+                   validate={[requiredField, minPasswordLength]}
                    type="password"
                    id="password"
                    name="password"
                    placeholder="Password"
             />
          </label>
-         <label htmlFor="rememberMe">
+         <label htmlFor="rememberMe" style={{ display: 'flex', alignContent: 'center' }}>
             <Field component={Input}
                    type="checkbox"
                    id="rememberMe"
