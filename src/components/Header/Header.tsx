3ed9fa725fb98caab4906c579f162072f36235dc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-26 22:04:06 +0300

    Added logOut button (78/100)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,18 @@ import s from './Header.module.css'
 
 import { HeaderPropsType } from './HeaderContainer'
 
-export const Header: FC<HeaderPropsType> = ({ isAuth, login }) => {
+export const Header: FC<HeaderPropsType> = ({ isAuth, login, logOut }) => {
+   const handleLogOut = () => logOut()
+
    return (
       <header className={s.header}>
          <img src="https://www.reactacademy.live/logoReact300px.png" alt="Logo" />
          <div className={s.loginBlock}>
             {isAuth ? (
-               <span>{login}</span>
+               <>
+                  <span>{login} <span>-</span> </span>
+                  <button onClick={handleLogOut}>Log Out</button>
+               </>
             ) : (
                <Link to="/login">Login</Link>
             )}
