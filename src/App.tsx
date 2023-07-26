4a3f30370f9ba0888d29c05397b59e98508a97b1
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-27 01:25:52 +0300

    refactor: replaced 'App' to class component, componentDidMount: getAuthUserData. MapStateToProps. (80/100)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { Component, ComponentType } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+
+import { authThunks } from 'redux/authReducer'
 
 import { Navbar } from 'components/Navbar/Navbar'
 import { News } from 'components/News/News'
@@ -12,31 +17,50 @@ import { LoginContainer } from 'components/Login/LoginContainer'
 
 import './styles/App.css'
 
-const App = () => {
-   return (
-      <div className="wrapper">
-         <HeaderContainer />
-         <Navbar />
-         <div className="app-wrapper-content">
-            <Switch>
-               <Route path="/profile/:userId?"
-                      render={() => <ProfileContainer />} />
-               <Route path="/messages"
-                      render={() => <MessagesContainer />} />
-               <Route path="/users"
-                      render={() => <UsersContainer />} />
-               <Route path="/news"
-                      render={() => <News />} />
-               <Route path="/music"
-                      render={() => <Music />} />
-               <Route path="/settings"
-                      render={() => <Settings />} />
-               <Route path="/login"
-                      render={() => <LoginContainer />} />
-            </Switch>
-         </div>
-      </div>
-   )
+const mapDispatchToProps = {
+   getAuthUserData: authThunks.getAuthUserData
 }
+
+export const App = compose<ComponentType>(
+   connect(null, mapDispatchToProps)
+)(class extends Component<AppPropsType> {
+   componentDidMount() {
+      const { getAuthUserData } = this.props
+
+      getAuthUserData()
+   }
+
+   render() {
+      return (
+         <div className="wrapper">
+            <HeaderContainer />
+            <Navbar />
+            <div className="app-wrapper-content">
+               <Switch>
+                  <Route path="/profile/:userId?"
+                         render={() => <ProfileContainer />} />
+                  <Route path="/messages"
+                         render={() => <MessagesContainer />} />
+                  <Route path="/users"
+                         render={() => <UsersContainer />} />
+                  <Route path="/news"
+                         render={() => <News />} />
+                  <Route path="/music"
+                         render={() => <Music />} />
+                  <Route path="/settings"
+                         render={() => <Settings />} />
+                  <Route path="/login"
+                         render={() => <LoginContainer />} />
+               </Switch>
+            </div>
+         </div>
+      )
+   }
+})
+
+// TYPES
+type MapDispatchPropsType = typeof mapDispatchToProps
+
+type AppPropsType = MapDispatchPropsType
 
 export default App
