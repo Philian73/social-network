675f5bce53aa33ef2de9672c735caff100a3aa0c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-26 22:00:51 +0300

    Added Redirect to profile (78/100)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,12 +4,16 @@ import { LoginParamsType } from 'api/authAPI'
 
 import { LoginReduxForm } from 'components/Login/LoginForm/LoginForm'
 import { LoginPropsType } from 'components/Login/LoginContainer'
+import { Redirect } from 'react-router-dom'
 
-export const Login: FC<LoginPropsType> = ({ logIn }) => {
+export const Login: FC<LoginPropsType> = ({ isAuth, logIn }) => {
    const onSubmit = (formData: LoginParamsType) => {
       logIn(formData)
    }
-   return (
+
+   return isAuth ? (
+      <Redirect to="/profile" />
+   ) : (
       <div>
          <h1>Login</h1>
          <LoginReduxForm onSubmit={onSubmit} />
