55d8e5c09a83febf969904b12532232c7922209f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-27 01:24:54 +0300

    refactor (80/100)

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -3,34 +3,30 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 
 import { AppStateType } from 'redux/store'
-import { authActions, authThunks } from 'redux/authReducer'
+import { authThunks } from 'redux/authReducer'
 
 import { Header } from './Header'
-
-class HeaderAPIContainer extends Component<HeaderPropsType> {
-   componentDidMount() {
-      const { getAuthUserData } = this.props
-
-      getAuthUserData()
-   }
-
-   render() {
-      return <Header {...this.props} />
-   }
-}
 
 const mapStateToProps = (state: AppStateType) => ({
    isAuth: state.auth.isAuth,
    login: state.auth.login,
 })
 
+const mapDispatchToProps = {
+   logOut: authThunks.logOut
+}
+
 export const HeaderContainer = compose<ComponentType>(
-   connect(mapStateToProps, { ...authActions, ...authThunks })
-)(HeaderAPIContainer)
+   connect(mapStateToProps, mapDispatchToProps)
+)(class extends Component<HeaderPropsType> {
+   render() {
+      return <Header {...this.props} />
+   }
+})
 
 
 // TYPES
 type MapStatePropsType = ReturnType<typeof mapStateToProps>
-type MapDispatchPropsType = typeof authActions & typeof authThunks
+type MapDispatchPropsType = typeof mapDispatchToProps
 
 export type HeaderPropsType = MapStatePropsType & MapDispatchPropsType
