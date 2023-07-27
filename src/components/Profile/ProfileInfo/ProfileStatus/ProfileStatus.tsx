97865856205114f1d6cd2060e2f6fe2c9ab52412
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-27 18:59:42 +0300

    refactor: class to Functional Component + logic (85/100)

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,62 +1,54 @@
-import { ChangeEvent, Component, FocusEvent } from 'react'
+import { ChangeEvent, FC, FocusEvent, useState } from 'react'
 import { UpdateStatusType } from 'redux/reducers/profileReducer'
 
-export class ProfileStatus extends Component<PropsType, StateType> {
-   state: StateType = {
-      editMode: false,
-      statusInput: this.props.status,
-   }
-
-   activateEditMode = () => {
-      if (this.state.editMode) return
-
-      this.setState({ editMode: true })
-   }
-
-   deactivateEditMode = () => {
-      if (!this.state.editMode) return
-
-      this.props.status !== this.state.statusInput && this.props.updateStatus(this.state.statusInput)
-      this.setState({ editMode: false })
-   }
-
-   onStatusInputFocus = (e: FocusEvent<HTMLInputElement>) => {
-      e.target.select()
-   }
-
-   onStatusInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-      this.setState({ statusInput: e.currentTarget.value })
-   }
-
-   render() {
-      const { status } = this.props
-      const { editMode, statusInput } = this.state
-      const { activateEditMode, deactivateEditMode, onStatusInputFocus, onStatusInputChange } = this
-
-      return !editMode ? (
-         <div>
-            <span onDoubleClick={activateEditMode}>{status}</span>
-         </div>
-      ) : (
-         <div>
-            <input value={statusInput}
-                   onChange={onStatusInputChange}
-                   autoFocus
-                   onFocus={onStatusInputFocus}
-                   onBlur={deactivateEditMode}
-            />
-         </div>
-      )
-   }
-}
-
-
-// TYPES
 type PropsType = {
    status: string
    updateStatus: UpdateStatusType
 }
-type StateType = {
-   editMode: boolean
-   statusInput: string
+
+export const ProfileStatus: FC<PropsType> = ({ status, updateStatus }) => {
+   const [editMode, setEditMode] = useState(false)
+   const [statusInput, setStatusInput] = useState(status)
+
+   const activateEditMode = () => {
+      if (editMode) return
+
+      if (status !== statusInput) {
+         setStatusInput(status)
+      }
+
+      setEditMode(true)
+   }
+
+   const deactivateEditMode = () => {
+      if (!editMode) return
+
+      if (status !== statusInput) {
+         updateStatus(statusInput)
+      }
+
+      setEditMode(false)
+   }
+
+   const onStatusInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setStatusInput(e.currentTarget.value)
+   }
+   const onStatusInputFocus = (e: FocusEvent<HTMLInputElement>) => {
+      e.target.select()
+   }
+
+   return !editMode ? (
+      <div>
+         <span onDoubleClick={activateEditMode}>{status}</span>
+      </div>
+   ) : (
+      <div>
+         <input value={statusInput}
+                onChange={onStatusInputChange}
+                autoFocus
+                onFocus={onStatusInputFocus}
+                onBlur={deactivateEditMode}
+         />
+      </div>
+   )
 }
